0d6cfcda2b7ec958bf3ec19a6e816606065d0bd5
iXendeRouS <aharrison9608@gmail.com>
2025-04-06 17:31:58 +1200

    added height weirdness warning

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,10 +3,11 @@ namespace TopDownCamera;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "47.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "TopDownCamera";
 
-    public const string Description = "Toggle between top down and normal viewing angle";
+    public const string Description = "Toggle between top down and normal viewing angle." +
+        "\nWARNING: this makes placement kinda weird on non-zero height areas so maybe build in normal mode and then swap to top down or something idk lol.";
 
     public const string RepoOwner = "iXendeRouS";
     public const string RepoName = "TopDownCamera";
