5adc11d62e3d5efea00fd55eb16947a543eb4925
iXendeRouS <aharrison9608@gmail.com>
2025-04-08 20:26:03 +1200

    updated version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace TopDownCamera;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "47.0";
-    public const string Version = "1.0.1";
+    public const string Version = "1.0.2";
     public const string Name = "TopDownCamera";
 
     public const string Description = "Toggle between top down and normal viewing angle." +
